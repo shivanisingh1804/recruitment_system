c5d38333d5b8fc5937d3f281ab227214354ea9c1
SHIVANI SINGH <167208267+shivanisingh1804@users.noreply.github.com>
2024-07-23 00:41:09 +0530

    main.go

--- main.go
+++ main.go
@@ -1,22 +1,4 @@
-// package main
 
-// import (
-// 	"log"
-// 	"net/http"
-// 	"recruitment-system/config"
-// 	"recruitment-system/routes"
-
-// 	"github.com/gorilla/mux"
-// )
-
-// func main() {
-// 	r := mux.NewRouter()
-// 	routes.RegisterRoutes(r)
-// 	config.ConnectDB()
-
-//		log.Println("Server running on port 3000")
-//		log.Fatal(http.ListenAndServe(":3000", r))
-//	}
 package main
 
 import (
